54742164011dc4044863119488f8cf8272296848
Andrés Pujol <andrespujol@gmail.com>
2023-05-05 15:22:12 -0300

    desafío manejo de archivos

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
+const fs = require('fs');
+
 class ProductManager {
-  constructor() {
+  constructor(filePath) {
     this.products = [];
     this.nextId = 1;
+    this.path = filePath;
   }
 
   addProduct(product) {
+    const products = JSON.parse(fs.readFileSync(this.path));
+  
     if (
       !product.code ||
-      !product.name ||
+      !product.title ||
       !product.price ||
       !product.description
     ) {
       throw new Error("Todos los campos son obligatorios");
     }
-    const existingProduct = this.products.find((p) => p.code === product.code);
+    const existingProduct = products.find((p) => p.code === product.code);
     if (existingProduct) {
       throw new Error("Ya existe un producto con ese código");
     }
@@ -21,16 +26,42 @@ class ProductManager {
       id: this.nextId++,
       ...product,
     };
-    this.products.push(newProduct);
-    return newProduct;
+    products.push(newProduct);
+    fs.writeFileSync(this.path, JSON.stringify(products));
   }
+  
 
   getProducts() {
-    return this.products;
+    const products = JSON.parse(fs.readFileSync(this.path));
+    return products;
+  }
+
+  updateProduct(id, newProductData) {
+    const products = JSON.parse(fs.readFileSync(this.path));
+  
+    const index = products.findIndex((p) => p.id === id);
+  
+    if (index !== -1) {
+      products[index] = {
+        ...products[index],
+        ...newProductData
+      };
+  
+      fs.writeFileSync(this.path, JSON.stringify(products));
+    }
+  }
+
+  deleteProduct(id) {
+    const products = JSON.parse(fs.readFileSync(this.path));
+  
+    const newProducts = products.filter((p) => p.id !== id);
+  
+    fs.writeFileSync(this.path, JSON.stringify(newProducts));
   }
 
   getProductById(id) {
-    const product = this.products.find((p) => p.id === id);
+    const products = JSON.parse(fs.readFileSync(this.path));
+    const product = products.find((p) => p.id === parseInt(id));
     if (!product) {
       throw new Error("Producto no encontrado");
     }
@@ -38,14 +69,16 @@ class ProductManager {
   }
 }
 
-const productManager = new ProductManager();
+const productManager = new ProductManager('productos.json');
 
 productManager.addProduct({
-  code: "P001",
-  name: "Producto de ejemplo",
-  price: 10.99,
-  description: "Descripción del producto de ejemplo",
+  "title": "Remera",
+  "description": "Remera de algodón",
+  "price": 2999.99,
+  "thumbnail": "https://i.ibb.co/6bddLrh/20201206-191301.jpg",
+  "code": "PA001",
+  "stock": 10
 });
-
+// productManager.deleteProduct(2)
 const products = productManager.getProducts();
 console.log(products);
